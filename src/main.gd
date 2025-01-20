605765e7e3a0eb9779e3928b4ea5cd6b948fdaef
DESKTOP-JMUNDKV\Brandon <bgudenius1@gmail.com>
2025-01-20 00:00:31 -0500

    added start of animation preview framework

--- src/main.gd
+++ src/main.gd
@@ -3,6 +3,7 @@ extends Control
 
 static var ae: FFTae
 static var rom:PackedByteArray = []
+static var global_fft_animation: FftAnimation = FftAnimation.new()
 
 @export var ui_manager: UiManager
 @export var load_rom_dialog: FileDialog
@@ -442,3 +443,300 @@ func _on_sprite_options_item_selected(_index: int) -> void:
 	draw_assembled_frame(11)
 	populate_frame_list(frame_list_container, shp)
 	#draw_assembled_frame(6)
+
+
+func start_animation(fft_animation: FftAnimation, draw_target: Sprite2D, is_playing: bool, isLooping: bool, force_loop: bool = false) -> void:
+	var num_parts: int = fft_animation.sequence.seq_parts.size()
+	
+	var only_opcodes: bool = true
+	for animation_part in fft_animation.sequence.seq_parts:
+		if not animation_part.isOpcode:
+			only_opcodes = false
+			break
+	
+	# don't loop when no parts, only 1 part, or all parts are opcodes
+	if (num_parts == 0 or only_opcodes): # TODO only_opcodes should play instead of showing a blank image, ie. if only a loop, but need to handle broken MON MFItem animation infinite loop
+		# draw a blank image
+		var assembled_image: Image = fft_animation.shp.create_blank_frame()
+		ui_manager.preview_viewport.sprite_primary.texture = ImageTexture.create_from_image(assembled_image)
+		await get_tree().create_timer(.001).timeout # prevent infinite loop from Wait opcodes looping only opcodes
+		return
+	elif (num_parts == 1 and not force_loop):
+		process_seq_part(fft_animation, 0, draw_target)
+		return
+	
+	if (is_playing):
+		await play_animation(fft_animation, draw_target, isLooping)
+	else:
+		process_seq_part(fft_animation, 0, draw_target)
+
+
+func play_animation(fft_animation: FftAnimation, draw_target: Sprite2D, isLooping: bool):
+	for animation_part_id:int in fft_animation.sequence.seq_parts.size():
+		var seq_part:SeqPart = fft_animation.sequence.seq_parts[animation_part_id]
+		# break loop animation when stopped or on selected animation changed to prevent 2 loops playing at once
+		if (isLooping and (!animation_is_playing 
+				or fft_animation != global_fft_animation)):
+			return
+		
+		await process_seq_part(fft_animation, animation_part_id, draw_target)
+		
+		if not seq_part.isOpcode:
+			var delay_frames: int = seq_part.parameters[1]  # param 1 is delay
+			var delay_sec: float = delay_frames / animation_speed
+			await get_tree().create_timer(delay_sec).timeout
+		
+	if isLooping:
+		play_animation(fft_animation, draw_target, isLooping)
+	else: # clear image when animation is over
+		draw_target.texture = ImageTexture.create_from_image(fft_animation.shp.create_blank_frame())
+
+
+func process_seq_part(fft_animation: FftAnimation, seq_part_id: int, draw_target:Node2D) -> void:
+	# print_debug(str(animation) + " " + str(animation_part_id + 3))
+	var seq_part:SeqPart = fft_animation.sequence.seq_parts[seq_part_id]
+	
+	var frame_id_label:String = ""
+	if seq_part.isOpcode:
+		frame_id_label = seq_part.to_string()
+	else:
+		frame_id_label = str(seq_part.parameters[0])
+	
+	#var new_anim_opcode_part_id: int = 0
+	if fft_animation.primary_anim_opcode_part_id == 0:
+		#primary_anim_opcode_part_id = fft_animation.sequence.seq_parts.size()
+		fft_animation.primary_anim_opcode_part_id = fft_animation.sequence.seq_parts.size()
+		#new_anim_opcode_part_id = fft_animation.sequence.seq_parts.size()
+	
+	# handle LoadFrameWait
+	if not seq_part.isOpcode:
+		var new_frame_id:int = seq_part.parameters[0]
+		var frame_id_offset:int = get_animation_frame_offset(fft_animation.weapon_frame_offset_index, fft_animation.shp)
+		new_frame_id = new_frame_id + frame_id_offset + opcode_frame_offset
+		frame_id_label = str(new_frame_id)
+	
+		if new_frame_id >= fft_animation.shp.frames.size(): # high frame offsets (such as shuriken) can only be used with certain animations
+			var assembled_image: Image = fft_animation.shp.create_blank_frame()
+			draw_target.texture = ImageTexture.create_from_image(assembled_image)
+		else:
+			var assembled_image: Image = fft_animation.shp.get_assembled_frame(new_frame_id, fft_animation.image, global_animation_id)
+			draw_target.texture = ImageTexture.create_from_image(assembled_image)
+			var rotation: float = fft_animation.shp.get_frame(new_frame_id, fft_animation.submerged_depth).y_rotation
+			if fft_animation.flipped_h:
+				rotation = -rotation
+			(draw_target.get_parent() as Node2D).rotation_degrees = rotation
+	
+	# only update ui for primary animation, not animations called through opcodes
+	if fft_animation.is_primary_anim:
+		animation_frame_slider.value = seq_part_id
+		frame_id_text.text = str(frame_id_label)
+	
+	var position_offset: Vector2 = Vector2.ZERO
+	
+	# Handle opcodes
+	if seq_part.isOpcode:
+		#print(anim_part_start)
+		if seq_part.opcode_name == "QueueSpriteAnim":
+			#print("Performing " + anim_part_start) 
+			if seq_part.parameters[0] == 1: # play weapon animation
+				var new_animation := FftAnimation.new()
+				var wep_file_name: String = Shp.shp_aliases["wep" + str(weapon_type)]
+				new_animation.seq = seqs[wep_file_name]
+				new_animation.shp = shps[wep_file_name]
+				new_animation.sequence = new_animation.seq.sequences[seq_part.parameters[1]]
+				new_animation.image = sprs["WEP.SPR"].spritesheet
+				new_animation.is_primary_anim = false
+				new_animation.flipped_h = fft_animation.flipped_h
+				
+				start_animation(new_animation, ui_manager.preview_viewport.sprite_weapon, true, false, false)
+			elif seq_part.parameters[0] == 2: # play effect animation
+				var new_animation := FftAnimation.new()
+				var eff_file_name: String = Shp.shp_aliases["eff" + str(effect_type)]
+				new_animation.seq = seqs[eff_file_name]
+				new_animation.shp = shps[eff_file_name]
+				new_animation.sequence = new_animation.seq.sequences[seq_part.parameters[1]]
+				new_animation.image = sprs["WEP.SPR"].spritesheet
+				new_animation.is_primary_anim = false
+				new_animation.flipped_h = fft_animation.flipped_h
+				
+				start_animation(new_animation, ui_manager.preview_viewport.sprite_effect, true, false, false)
+			else:
+				print_debug("Error: QueueSpriteAnim: " + seq_part.to_string() + "\n" + fft_animation.sequence.to_string())
+				push_warning("Error: QueueSpriteAnim: " + seq_part.to_string() + "\n" + fft_animation.sequence.to_string())
+		elif seq_part.opcode_name.begins_with("Move"):
+			if seq_part.opcode_name == "MoveUnitFB":
+				position_offset = Vector2(-(seq_part.parameters[0]), 0) # assume facing left
+			elif seq_part.opcode_name == "MoveUnitDU":
+				position_offset = Vector2(0, seq_part.parameters[0])
+			elif seq_part.opcode_name == "MoveUnitRL":
+				position_offset = Vector2(seq_part.parameters[0], 0)
+			elif seq_part.opcode_name == "MoveUnitRLDUFB":
+				position_offset = Vector2((seq_part.parameters[0]) - (seq_part.parameters[2]), seq_part.parameters[1]) # assume facing left
+			elif seq_part.opcode_name == "MoveUp1":
+				position_offset = Vector2(0, -1)
+			elif seq_part.opcode_name == "MoveUp2":
+				position_offset = Vector2(0, -2)
+			elif seq_part.opcode_name == "MoveDown1":
+				position_offset = Vector2(0, 1)
+			elif seq_part.opcode_name == "MoveDown2":
+				position_offset = Vector2(0, 2)
+			elif seq_part.opcode_name == "MoveBackward1":
+				position_offset = Vector2(1, 0) # assume facing left
+			elif seq_part.opcode_name == "MoveBackward2":
+				position_offset = Vector2(2, 0) # assume facing left
+			elif seq_part.opcode_name == "MoveForward1":
+				position_offset = Vector2(-1, 0) # assume facing left
+			elif seq_part.opcode_name == "MoveForward2":
+				position_offset = Vector2(-2, 0) # assume facing left
+			else:
+				print_debug("can't inerpret " + seq_part.opcode_name)
+				push_warning("can't inerpret " + seq_part.opcode_name)
+			
+			if fft_animation.flipped_h:
+				position_offset = Vector2(-position_offset.x, position_offset.y)
+			(draw_target.get_parent().get_parent() as Node2D).position += position_offset
+		elif seq_part.opcode_name == "SetLayerPriority":
+			# print(layer_priority_table)
+			var layer_priority: Array = layer_priority_table[seq_part.parameters[0]]
+			for i in range(0, layer_priority.size() - 1):
+				var layer_name = layer_priority[i + 1] # skip set_id
+				if layer_name == "unit":
+					ui_manager.preview_viewport.sprite_primary.z_index = -i
+				elif layer_name == "weapon":
+					ui_manager.preview_viewport.sprite_weapon.z_index = -i
+				elif layer_name == "effect":
+					ui_manager.preview_viewport.sprite_effect.z_index = -i
+				elif layer_name == "text":
+					ui_manager.preview_viewport.sprite_text.z_index = -i
+		elif seq_part.opcode_name == "SetFrameOffset":
+			opcode_frame_offset = seq_part.parameters[0] # use global var since SetFrameOffset is only used in animations that do not call other animations
+		elif seq_part.opcode_name == "FlipHorizontal":
+			ui_manager.preview_viewport.sprite_primary.flip_h = !ui_manager.preview_viewport.sprite_primary.flip_h
+		elif seq_part.opcode_name == "UnloadMFItem":
+			var target_sprite = ui_manager.preview_viewport.sprite_item
+			target_sprite.texture = fft_animation.shp.create_blank_frame()
+			# reset any rotation or movement
+			(target_sprite.get_parent() as Node2D).rotation_degrees = 0
+			(target_sprite.get_parent() as Node2D).position = Vector2(0,0)
+		elif seq_part.opcode_name == "MFItemPosFBDU":
+			var target_sprite_pivot := ui_manager.preview_viewport.sprite_item.get_parent() as Node2D
+			target_sprite_pivot.position = Vector2(-(seq_part.parameters[0]), (seq_part.parameters[1]) + 20) # assume facing left, add 20 because it is y position from bottom of unit
+		elif seq_part.opcode_name == "LoadMFItem":
+			var item_frame_id:int = item_index # assumes loading item
+			var item_sheet_type:Shp = shps["item"]
+			#var item_cel_image = ExtensionsApi.project.get_cel_at(ExtensionsApi.project.current_project, item_frame, item_layer)
+			var item_image = sprs["ITEM"].spritesheet
+			
+			if item_index >= 180:
+				item_sheet_type = shps["other"]
+				#item_cel_image = ExtensionsApi.project.get_cel_at(ExtensionsApi.project.current_project, other_frame, other_layer)
+				item_image = sprs["OTHER.SPR"].spritesheet
+				
+				if item_index <= 187: # load crystal
+					item_frame_id = item_index - 179
+					other_type_selector.select(2) # to update ui
+					other_type_index = 2 # to set v_offset is correct
+				elif item_index == 188: # load chest 1
+					item_frame_id = 15
+					other_type_selector.select(0)
+					other_type_index = 0
+				elif item_index == 189: # load chest 2
+					item_frame_id = 16
+					other_type_selector.select(0)
+					other_type_index = 0
+			
+			frame_id_label = str(item_index)
+			
+			var assembled_image: Image = item_sheet_type.get_assembled_frame(item_frame_id, item_image)
+			var target_sprite = ui_manager.preview_viewport.sprite_item
+			target_sprite.texture = ImageTexture.create_from_image(assembled_image)
+			var rotation: float = item_sheet_type.get_frame(item_frame_id, submerged_depth_options.selected).y_rotation
+			(target_sprite.get_parent() as Node2D).rotation_degrees = rotation
+		elif seq_part.opcode_name == "Wait":
+			var loop_length: int = seq_part.parameters[0]
+			var num_loops: int = seq_part.parameters[1]
+			
+			var primary_animation_part_id = seq_part_id + fft_animation.primary_anim_opcode_part_id - fft_animation.sequence.seq_parts.size()
+			# print_debug(str(primary_animation_part_id) + "\t" + str(animation_part_id) + "\t" + str(primary_anim_opcode_part_id) + "\t" + str(animation.size() - 3))
+			
+			var temp_seq: Sequence = get_sub_animation(loop_length, primary_animation_part_id, fft_animation.parent_anim.sequence)
+			var temp_fft_animation: FftAnimation = fft_animation.get_duplicate()
+			temp_fft_animation.sequence = temp_seq
+			temp_fft_animation.parent_anim = fft_animation
+			temp_fft_animation.is_primary_anim = false
+			temp_fft_animation.primary_anim_opcode_part_id = primary_animation_part_id
+			
+			for iteration in num_loops:
+				await start_animation(temp_fft_animation, draw_target, true, false, true)
+			
+		elif seq_part.opcode_name == "IncrementLoop":
+			pass # handled by animations looping by default
+		elif seq_part.opcode_name == "WaitForInput":
+			var delay_frames = wait_for_input_delay
+			var loop_length: int = seq_part.parameters[0]
+			var primary_animation_part_id = seq_part_id + fft_animation.primary_anim_opcode_part_id - fft_animation.sequence.seq_parts.size()
+			var temp_seq: Sequence = get_sub_animation(loop_length, primary_animation_part_id, fft_animation.parent_anim.sequence)
+			var temp_fft_animation: FftAnimation = fft_animation.get_duplicate()
+			temp_fft_animation.sequence = temp_seq
+			temp_fft_animation.parent_anim = fft_animation
+			temp_fft_animation.is_primary_anim = false
+			temp_fft_animation.primary_anim_opcode_part_id = primary_animation_part_id
+			
+			# print_debug(str(temp_anim))
+			var timer: SceneTreeTimer = get_tree().create_timer(delay_frames / animation_speed)
+			while timer.time_left > 0:
+				# print(str(timer.time_left) + " " + str(temp_anim))
+				await start_animation(temp_fft_animation, draw_target, true, false, true)
+		elif seq_part.opcode_name.begins_with("WeaponSheatheCheck"):
+			var delay_frames = weapon_sheathe_check1_delay
+			if seq_part.opcode_name == "WeaponSheatheCheck2":
+				delay_frames = weapon_sheathe_check2_delay
+			
+			var loop_length: int = seq_part.parameters[0]
+			var primary_animation_part_id = seq_part_id + fft_animation.primary_anim_opcode_part_id - fft_animation.sequence.seq_parts.size()
+			# print_debug(str(primary_animation_part_id) + "\t" + str(animation_part_id) + "\t" + str(primary_anim_opcode_part_id) + "\t" + str(animation.size() - 3))
+			
+			var temp_seq: Sequence = get_sub_animation(loop_length, primary_animation_part_id, fft_animation.parent_anim.sequence)
+			var temp_fft_animation: FftAnimation = fft_animation.get_duplicate()
+			temp_fft_animation.sequence = temp_seq
+			temp_fft_animation.parent_anim = fft_animation
+			temp_fft_animation.is_primary_anim = false
+			temp_fft_animation.primary_anim_opcode_part_id = primary_animation_part_id
+			
+			# print_debug(str(temp_anim))
+			var timer: SceneTreeTimer = get_tree().create_timer(delay_frames / animation_speed)
+			while timer.time_left > 0:
+				await start_animation(temp_fft_animation, draw_target, true, false, true)
+		elif seq_part.opcode_name == "WaitForDistort":
+			pass
+		elif seq_part.opcode_name == "QueueDistortAnim":
+			# https://ffhacktics.com/wiki/Animate_Unit_Distorts
+			pass
+
+
+func get_animation_frame_offset(weapon_frame_offset_index:int, shp:Shp) -> int:
+	if ((shp.file_name.contains("wep") or shp.file_name.contains("eff"))
+		and shp.zero_frames.size() > 0):
+		return shp.zero_frames[weapon_frame_offset_index]
+	else:
+		return 0
+
+
+func get_sub_animation(length:int, sub_animation_end_part_id:int, parent_animation:Sequence) -> Sequence:
+	var sub_anim_length: int = 0
+	var sub_anim: Sequence = Sequence.new()
+	var previous_anim_part_id = sub_animation_end_part_id - 1
+	
+	# print_debug(str(animation) + "\n" + str(previous_anim_part_id))
+	while sub_anim_length < abs(length):
+		# print_debug(str(previous_anim_part_id) + "\t" + str(sub_anim_length) + "\t" + str(parent_animation[previous_anim_part_id + 3]) + "\t" + str(parent_animation[sub_animation_end_part_id + 3][0]))
+		var previous_anim_part: SeqPart = parent_animation.seq_parts[previous_anim_part_id]
+		sub_anim.seq_parts.insert(0, previous_anim_part)
+		sub_anim_length += previous_anim_part.length
+	
+		previous_anim_part_id -= 1
+	
+	# add label, id, and num_parts
+	sub_anim.seq_name = parent_animation.seq_name + ":" + str(sub_animation_end_part_id - length) + "-" + str(sub_animation_end_part_id)
+	
+	return sub_anim
